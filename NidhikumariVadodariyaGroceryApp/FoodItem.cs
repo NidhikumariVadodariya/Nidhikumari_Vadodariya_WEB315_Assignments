b32e64e350b0ab31aa8ee418519c9aae8b3b5b83
NidhikumariVadodariya <NV29@myscc.ca>
2022-03-24 09:27:49 -0400

    Deleted assignment 1 from Assignment 2
    Deleted assignment 1 from Assignment 2

---
+++ NidhikumariVadodariyaGroceryApp/FoodItem.cs
@@ -0,0 +1,8 @@
+public class Fooditem
+{
+    public decimal goods_and_services_tax { get; set; }
+    public string Potato_Chips  { get; set; }
+    public string Chocolates  { get; set; }    
+    public string Maggie  { get; set; }    
+
+}
---
+++ NidhikumariVadodariyaGroceryApp/GroceryIsle.cs
@@ -0,0 +1,7 @@
+public class GroceryIsle
+{
+    public string List_of_FoodItems { get; set; }
+    public string isle_name  { get; set; }
+    public string isle_number  { get; set; }
+
+}
